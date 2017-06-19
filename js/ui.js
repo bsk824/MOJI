c1bc0c44ae04035c655371060b66067dface7dca
seungki <bsk824@gmail.com>
2017-06-19 15:16:28 +0900

    스크립트 작업

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,5 @@
 var winW = $(window).width();
-    
+
 function fontSize(winW) {
     var fontSize = 0;
     (winW < 560) ? fontSize = winW / 5.12 : fontSize = winW / 9.04;
@@ -7,6 +7,41 @@ function fontSize(winW) {
 }
 fontSize(winW);
 
+function toggleActive(obj) {
+    var btn = $(obj);
+    btn.click(function(){
+        var _this = $(this);
+        _this.toggleClass('active');
+    });
+}
+function point(obj) {
+    var wrap = $(obj),
+        point = wrap.find('.point'),
+        sel = wrap.find('select');
+
+    sel.change(function(){
+        var val = $(this).val();
+        point.removeClass().addClass('point'+val).text(val+'점');
+    })
+}
+function tab(btn, cont) {
+    var tabBtn = $(btn),
+        tabCont = $(cont).find('.tabCont');
+    tabBtn.click(function(){
+        var _this = $(this),
+            idx = _this.index();
+        _this.addClass('active').siblings().removeClass('active');
+        tabCont.eq(idx).addClass('active').siblings().removeClass('active');
+    })
+}
+function accodian(obj) {
+    var btn = $(obj).find('.tit');
+
+    btn.click(function(){
+        var _this = $(this);
+        _this.parent().addClass('current').siblings().removeClass('current');
+    })
+}
 $(window).resize(function(){
     var winW = $(window).width();
     fontSize(winW);
